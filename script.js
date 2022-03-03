e5871f4a42b1a99a17d88189ab39bf97accf5ce0
Sofiane B <95911794+sofianeb722@users.noreply.github.com>
2022-03-03 10:25:48 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function TangoDeux( ) {
+var question = prompt('La fin quand le soleil se lèvera à l\'\est ou à l\'\ouest ?');
+
+if (question == "Est") {
+    alert('Destruction imminente');
+
+}
+if (question == "Ouest") {
+    alert('Destruction instantanée');
+}
+else {
+    alert('Tu devrais apprendre à lire dans l\'\avenir')
+    function TangoDeux
+    }
+}
+TangoDeux()
